0944fc2f7836848e4c8bf3f483e05f05e4e24430
Zero <1300326388@qq.com>
2020-07-10 19:39:20 +0800

    1、更新版本号和去掉不必要文件

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -2,10 +2,12 @@
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/zero/sdk/flutter"
 export "FLUTTER_APPLICATION_PATH=/Users/zero/project/github/flutter_record_mp3/example"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_TARGET=/Users/zero/project/github/flutter_record_mp3/example/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
 export "FLUTTER_FRAMEWORK_DIR=/Users/zero/sdk/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=5"
+export "TRACK_WIDGET_CREATION=true"
+export "DART_DEFINES=flutter.inspector.structuredErrors=true"
