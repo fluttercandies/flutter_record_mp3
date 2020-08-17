be6dabb03680fda69ac5a29a5385509a882a0738
shingohu <shingohu@163.com>
2020-08-17 16:12:03 +0800

    add support swift version 5.0

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -2,9 +2,10 @@
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/hujie/MEGA/Flutter/tool/flutter"
 export "FLUTTER_APPLICATION_PATH=/Users/hujie/MEGA/Flutter/my_flutter_plugins/record_mp3/example"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_TARGET=/Users/hujie/MEGA/Flutter/my_flutter_plugins/record_mp3/example/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_FRAMEWORK_DIR=/Users/hujie/MEGA/Flutter/tool/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=5"
+export "TRACK_WIDGET_CREATION=true"
