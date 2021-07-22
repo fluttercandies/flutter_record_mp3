803a169f92f5deb6f11f79a8185a0a8c7cf8665a
MaJialun <mjl0602@gmail.com>
2021-07-22 19:29:51 +0800

    Update to nullsafety

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,17 +1,14 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-
-export "FLUTTER_ROOT=/Users/hujie/MEGA/Flutter/tool/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/hujie/MEGA/Flutter/my_flutter_plugins/record_mp3/example"
-export "FLUTTER_TARGET=/Users/hujie/MEGA/Flutter/my_flutter_plugins/record_mp3/example/lib/main.dart"
-
+export "FLUTTER_ROOT=/Users/majialun/Documents/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/majialun/Documents/code/github/flutter_record_mp3/example"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
-export "FLUTTER_FRAMEWORK_DIR=/Users/zero/sdk/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=5"
-export "TRACK_WIDGET_CREATION=true"
-
-export "DART_DEFINES=flutter.inspector.structuredErrors=true"
-
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.packages"
